eb99a97805911efb0655bc12f7ea91e159c9efe1
AkshayMore93 <akki.more93@gmail.com>
2023-10-22 22:36:19 +0530

    Initial project setup

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "include/HelloWorld.h"
+
+
+int main()
+{
+	PrintHelloWorld();
+}
---
+++ src/HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "../include/HelloWorld.h"
+
+
+void PrintHelloWorld()
+{
+	std::cout<<"Hello World"<<std::endl;
+}
